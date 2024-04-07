62dfd034bbc5cc84072111f7bc5485c21739b28c
itsdevsyed <165158831+itsdevsyed@users.noreply.github.com>
2024-04-08 01:12:31 +0530

    new file to upload

--- proxy_parse.c
+++ proxy_parse.c
@@ -3,3 +3,11 @@
 #define DEFAULT_NHDRS 8
 #define MAX_REQ_LEN 65535
 #define MIN_REQ_LEN 4
+
+static const char *root_abs_path = "/";
+
+/* private function declartions */
+int ParsedRequest_printRequestLine(struct ParsedRequest *pr, 
+				   char * buf, size_t buflen,
+				   size_t *tmp);
+size_t ParsedRequest_requestLineLen(struct ParsedRequest *pr);
